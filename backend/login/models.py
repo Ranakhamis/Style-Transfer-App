730e48d175a5d607d29cf2f15701c8c4f3cfbfe9
mostafax <mostafamousa1080p@gmail.com>
2020-05-02 22:35:54 +0200

    Refactoring the repo

--- backend/login/models.py
+++ backend/login/models.py
@@ -27,3 +27,35 @@ def add_user(name, email, profile_image, age, password):
     users.insert(user)
 
     return True
+
+
+def user_valid(email, password):
+    users = db.users
+    user = users.find({'email': email})
+    if user.count() > 0 and user[0]['password'] == password:
+        return True
+    else:
+        return False
+
+
+def get_user_data(email):
+    """
+    Gets a specific user articles.
+    :param email: User email.
+    :return: Dictionary containing User"""
+    user = db.users.find({'email': email})[0]
+    return user
+
+
+def get_session_email(session_id):
+    return db.Sessions.find({'_id': ObjectId(session_id)})[0]['user_email']
+
+
+def get_user_images(session_id):
+    email = db.Sessions.find({'_id': ObjectId(session_id)})[0]['user_email']
+    return db.users.find({'email': email})[0]['images']
+
+
+def add_user_image(user_email):
+    user = db.users.find({'email': user_email})[0]
+    
--- backend/login/views.py
+++ backend/login/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from login.models import add_user, add_session, get_user_data, user_valid
+
 
 # Create your views here.
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'stories/registration.html')
+
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    age = request.POST.get('age')
+    profile_image = request.POST.get('profile_image')
+    if add_user(name, email, profile_image, age, password):
+        session_id = add_session(email)
+        data = get_user_data(email)
+        data.update({'session_id': session_id})
+        return render(request, 'stories/profile.html', data)
+    else:
+        return HttpResponse('Email Already Used.')
+
+
+def login(request):
+    """
+    Authenticates user and redirects to user profile.
+    :return:
+        - User profile page if authentication succeeds.
+        - Login page with error msg dict.
+    """
+    email = request.POST['email']
+    password = request.POST['password']
+    if user_valid(email, password):
+        session_id = add_session(email)
+        data = get_user_data(email)
+        data.update({'session_id': session_id})
+        return render(request, 'stories/profile.html', data)
+    else:
+        return render(request, 'stories/login.html',
+                      {'response_message': 'Login Failed.'})
+
