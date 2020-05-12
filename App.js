92eb0a515e00f5759796e503535377ead17b0204
Rana <ranakhms@gmail.com>
2020-05-12 06:24:18 +0200

    publishing the map

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView
+    initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    }}
+    style={{  // required style for map appearance
+        position: "absolute", 
+        top: 0, 
+        bottom: 0, 
+        right: 0, 
+        left: 0
+    }}
+/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
