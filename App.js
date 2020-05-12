c4d0678dba0afd0f802ef6c3c34ababf7273bccc
Rana <ranakhms@gmail.com>
2020-05-13 00:59:05 +0200

    Updating geolocation in map view component

--- App.js
+++ App.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView ,{Marker} from 'react-native-maps';
+import { recoveredProps } from 'expo-error-recovery';
+import * as Permissions from 'expo-permissions';
+
+
 
 export default class App extends React.Component {
+
+  constructor (Props){
+    super(Props);
+    this.state = {
+      latitude :0,
+      longitude:0,
+      error: null,
+
+    }
+  } 
+  
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(position=> {
+      this.setState({
+        latitude : position.coords.latitude,
+        longitude: position.coords.longitude,
+        error: null,
+      });
+    }, error=> this.setState({error: error.message}),
+      {enableHighAccuracy : true, 
+      timeout: 2000, maximumAge: 2000} 
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -10,26 +38,23 @@ export default class App extends React.Component {
     initialRegion={{
         latitude: 30.044281,
         longitude: 31.340002,
-        
+        latitudeDelta: 0.195,
+        longitudeDelta: 0.2,
+        //cairo
     }}
     style={{  // required style for map appearance
         position: "absolute", 
         top: 0, 
         bottom: 0, 
         right: 0, 
-        left: 0
+        left: 0,
     }}
 />
+    <Marker coordinate={this.state} title={Marker.title}description={Marker.description}
+/>    
       </View>
     );
   }
-}
-export default function App() {
-  return (
-    <View style={styles.container}>
-     
-    </View>
-  );
 }
 
 const styles = StyleSheet.create({
